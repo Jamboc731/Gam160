68287ab3cee82a73d1e4acf76e2bf43c7fc10bf5
Jamboc731 <jamiec.covellj@gmail.com>
2019-03-25 16:50:02 +0000

    Fuck manny

--- Assets/Scripts/GetValues.cs
+++ Assets/Scripts/GetValues.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class GetValues : MonoBehaviour
 {
-
+    
     [SerializeField] Vector3 rightShoulderVal;
     [SerializeField] Vector3 leftShoulderVal;
     [SerializeField] float rightElbowVal;
     [SerializeField] float leftElbowVal;
     [SerializeField] Vector3 rightHandVal;
     [SerializeField] Vector3 leftHandVal;
-
-    [SerializeField] GameObject [] body = new GameObject [6];
+    
+    [SerializeField] GameObject [] body = new GameObject [6]; //values seem to be RS LS RE RH LH T LE
 
     private void Start ()
     {
