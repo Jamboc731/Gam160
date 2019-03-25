cb0826fc8de3a36341b65fb5a5a300f9f9db810c
Jamboc731 <jamiec.covellj@gmail.com>
2019-03-25 16:47:36 +0000

    initial

---
+++ Assets/Scripts/GetValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetValues : MonoBehaviour
+{
+
+    [SerializeField] Vector3 rightShoulderVal;
+    [SerializeField] Vector3 leftShoulderVal;
+    [SerializeField] float rightElbowVal;
+    [SerializeField] float leftElbowVal;
+    [SerializeField] Vector3 rightHandVal;
+    [SerializeField] Vector3 leftHandVal;
+
+    [SerializeField] GameObject [] body = new GameObject [6];
+
+    private void Start ()
+    {
+        body = GameObject.FindGameObjectsWithTag ("Body");
+
+        foreach(var b in body)
+        {
+            Debug.Log (b.name);
+        }
+
+    }
+
+
+
+}
